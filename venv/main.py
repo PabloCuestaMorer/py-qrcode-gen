fd7c4f2328fe62cc19b65e5ebdc4d97cdf4fc681
pcuestamorer <pablocuestam@outlook.com>
2023-07-25 22:15:50 +0200

    create an enviroment

---
+++ venv/main.py
@@ -0,0 +1,39 @@
+from flask import Flask, render_template, request
+import qrcode
+from io import BytesIO
+from base64 import b64encode
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/", methods=["POST"])
+def generateQR():
+    data = request.form.get("qrInput")
+
+    # Input validation
+    if not data:
+        return render_template("index.html", error="ERROR: Please enter a valid input.")
+
+    try:
+        qrImg = qrcode.make(data)
+    except Exception as e:
+        return render_template(
+            "index.html", error="Failed to generate QR code. Please try again later."
+        )
+
+    memory = BytesIO()
+    qrImg.save(memory)
+    memory.seek(0)
+    b64EncodeQrImg = "data:image/png;base64," + b64encode(memory.getvalue()).decode(
+        "ascii"
+    )
+    return render_template("index.html", data=b64EncodeQrImg)
+
+
+#if __name__ == "__main__":
+#    app.run(debug=True, host="0.0.0.0", port=8080)
